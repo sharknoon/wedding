48f207e43a9b532de05bd46be1362b375302b2d0
Josua Frank <frank.josua@gmail.com>
2021-11-09 23:25:57 +0100

    Added personal schedule

--- src/routes/prints/schedule.svelte
+++ src/routes/prints/schedule.svelte
@@ -1,3 +1,147 @@
+<script lang="ts">
+	import { each } from 'svelte/internal';
+
+	const milestones = [
+		{
+			date: new Date(2016, 2, 29),
+			title: 'Begegnung',
+			image: `<svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 flex-grow" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+</svg>`
+		},
+		{
+			date: new Date(2017, 6, 16),
+			title: 'Erstes Date',
+			image: `<svg class="h-10 w-10 flex-grow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M279.1 279.6l222.1-222C523.3 36.38 508.3 0 478.3 0H33.72c-30 0-45 36.38-23.75 57.63L128 175.7V176h.3359l103.6 103.6V463L220.8 464H152C138.8 464 128 474.8 128 488S138.8 512 152 512h208c13.25 0 24-10.75 24-24s-10.75-24-24-24h-78.09c-1.701-.2207-3.203-1-4.965-1h3.02V279.6zM443.7 48l-80 80h-215.5l-80-80H443.7zM255.1 235.8L196.2 176h119.5L255.1 235.8z"></path></svg>`
+		},
+		{
+			date: new Date(2017, 7, 15),
+			title: 'Verliebt',
+			image: `<svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 flex-grow" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+</svg>`
+		},
+		{
+			date: new Date(2017, 11, 17),
+			title: 'Paar',
+			image: `<svg class="h-10 w-10 flex-grow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M159.1 96c26.5 0 47.1-21.5 47.1-48S186.5 0 160 0C133.5 0 111.1 21.5 111.1 48S133.5 96 159.1 96zM479.1 96c26.5 0 48-21.5 48-48S506.5 0 479.1 0c-26.5 0-47.1 21.5-47.1 48S453.5 96 479.1 96zM242.4 172.6C231.3 145.5 205.2 128 175.8 128H144.2C114.8 128 88.71 145.5 77.56 172.6L33.81 278.9C28.75 291.1 34.59 305.1 46.87 310.2c2.975 1.242 6.066 1.832 9.113 1.832c9.4 0 18.38-5.604 22.2-14.88l17.81-43.27V488c0 13.25 10.75 24 24 24s23.1-10.75 23.1-24V352h32v136c0 13.25 10.75 24 24 24c13.25 0 23.99-10.75 23.99-24V253.9l17.81 43.27C245.6 306.4 254.6 312 263.1 312c3.062 0 6.156-.5781 9.123-1.812c12.28-5.047 18.12-19.06 13.06-31.33L242.4 172.6zM143.1 304v-128h31.1l.002 128H143.1zM606.2 278.9l-43.75-106.3C551.3 145.5 525.2 128 495.8 128h-31.69c-29.31 0-55.44 17.5-66.59 44.58l-43.75 106.3c-5.062 12.27 .7813 26.28 13.06 31.33c2.877 1.205 5.846 1.771 8.764 1.771c7.957 0 15.57-4.205 20.09-11.14L367.1 384h47.1v104c0 13.25 10.75 24 24 24s23.1-10.75 23.1-24V384h31.1v104c0 13.25 10.75 24 24 24c13.25 0 23.99-10.75 23.99-24V384h48.01l-27.43-82.26C569.1 308.1 576.3 312 583.1 312c3.062 0 6.156-.5781 9.125-1.812C605.4 305.1 611.2 291.1 606.2 278.9zM434.6 336l45.4-136.2L525.4 336H434.6z"></path></svg>`
+		},
+		{
+			date: new Date(2021, 7, 22),
+			title: 'Verlobung',
+			image: `<svg class="h-10 w-10 flex-grow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 64C109.1 64 0 125.9 0 208v98.13C0 384.5 114.6 448 256 448s256-63.5 256-141.9V208C512 125.9 401.1 64 256 64zM256 112c110.5 0 200 35.88 200 80c0 9.125-4 17.75-11 25.88C398.2 192.2 331 176 256 176S113.8 192.2 67 217.9C60 209.8 56 201.1 56 192C56 147.9 145.5 112 256 112zM397.8 248.5C361.5 263 311.4 272 256 272S150.5 263 114.2 248.5C149.1 234.2 198.4 224 256 224S361.1 234.2 397.8 248.5zM464 306.1C464 344.4 383 400 256 400s-208-55.63-208-93.88V266.4C94.38 298.8 170.1 320 256 320s161.6-21.25 208-53.63V306.1z"></path></svg>`
+		},
+		{
+			date: new Date(2023, 8, 15),
+			title: 'Heirat',
+			image: `<svg class="h-10 w-10 flex-grow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M371.9 147.7c10.13 18.5 17.75 38.37 22.38 59c41.13 21.25 69.63 63.75 69.63 113.2c0 70.75-57.25 128-128 128s-128-57.25-128-128c0-54.63 34.5-101.1 82.88-119.5c8.5 17.25 13 36.25 13.13 55.5c0 37.37-16.38 70.75-42 94.12c6.25 15.25 17.13 28.12 31 37c44.75-39.75 66.13-99.63 56.75-158.7c-9.375-59.25-48.25-109.5-103.1-133.5l41.38-66.25L232.4-16H119.4l-55.5 44.62l41.25 66.25C29.03 128.4-13.35 210.6 3.777 292.1s88.88 139.8 172.1 139.9c7.625-.125 15.25-.625 22.88-1.875c48.5 60.5 131 82.13 203 53.12c71.88-28.87 116.4-101.6 109.5-178.9S447.8 163.5 371.9 147.7zM127.9 39.37l8.5-7.375h79l8.5 7.375l-25.5 42.25c-14.88-2.125-30.13-2.125-45 0L127.9 39.37zM47.9 255.1c.125-70.62 57.38-127.9 128-128c33 0 62.88 12.88 85.63 33.5c-59.88 28-101.6 88-101.6 158.5c0 21.75 4.125 43.38 12 63.63C103.3 381.5 47.9 325.2 47.9 255.1z"></path></svg>`
+		}
+	];
+	const monthNames = [
+		'Januar',
+		'Februar',
+		'März',
+		'April',
+		'Mai',
+		'Juni',
+		'Juli',
+		'August',
+		'September',
+		'Oktober',
+		'November',
+		'Dezember'
+	];
+</script>
+
+<div class="shadow-2xl h-[56rem] w-[42rem] p-1 bg-white">
+	<div class="border-2 border-gray-900 border-dashed h-full flex flex-col p-4">
+		<div class="flex-grow flex flex-col justify-center items-center gap-8 px-12">
+			<div class="grid grid-rows-1 grid-cols-2 w-[21rem] h-[21rem]">
+				<div
+					class="col-start-1 row-start-1 image bg-cover bg-center bg-no-repeat filter grayscale"
+				/>
+				<div class="col-start-2 row-start-1 flex flex-col">
+					<div
+						class="flex-1 flex justify-center items-end text-center text-4xl uppercase font-cheap-pine-sans tracking-widest"
+					>
+						B l a c k
+					</div>
+					<div
+						class="flex-1 bg-black text-white text-center text-4xl uppercase font-cheap-pine-sans tracking-widest"
+					>
+						W h i t e
+					</div>
+				</div>
+			</div>
+			<div class="self-stretch flex gap-4 px-4 mx-12 items-center">
+				<div class="flex-grow border-t-2 border-black" />
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-10 w-10"
+					viewBox="0 0 20 20"
+					fill="rgb(220,38,38)"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+				<div class="flex-grow border-t-2 border-black" />
+			</div>
+			<div
+				class="text-center uppercase font-cheap-pine-sans tracking-wider leading-4 pb-1 text-2xl"
+			>
+				Gesucht und Gefunden, in Liebe verbunden
+			</div>
+		</div>
+		<hr class="border border-black my-1" />
+		<hr class="border-[3px] border-black my-1" />
+		<div class="flex items-center divide-x-2 divide-black my-3">
+			<span class="text-8xl font-cheap-pine pr-2">23</span>
+			<div class="flex flex-col text-center px-2">
+				<span
+					class="font-cheap-pine-sans text-4xl tracking-widest leading-none bg-black text-white px-1"
+					>September</span
+				>
+				<span class="font-cheap-pine-sans text-6xl tracking-wider font-bold leading-[0.85]"
+					>2 0 2 3</span
+				>
+			</div>
+			<div class="flex-grow flex flex-col text-center">
+				<span class="font-cheap-pine-sans text-4xl tracking-widest leading-none">
+					Midrène's und Josua's
+				</span>
+				<div class="flex items-center justify-between px-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="text-red-600 h-9 w-9"
+						viewBox="0 0 20 20"
+						fill="currentcolor"
+					>
+						<path d="M20 7h-7L10 .5 7 7H0l5.46 5.47-1.64 7 6.18-3.7 6.18 3.73-1.63-7z" />
+					</svg>
+					<span class="font-cheap-pine text-6xl tracking-widest"> Hochzeit </span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="text-red-600 h-9 w-9"
+						viewBox="0 0 20 20"
+						fill="currentcolor"
+					>
+						<path d="M20 7h-7L10 .5 7 7H0l5.46 5.47-1.64 7 6.18-3.7 6.18 3.73-1.63-7z" />
+					</svg>
+				</div>
+			</div>
+		</div>
+		<span class="text-center text-gray-700 tracking-widest">
+			x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;
+			x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;
+			x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x
+		</span>
+	</div>
+</div>
+
 <div class="shadow-2xl h-[56rem] w-[42rem] p-1 bg-white">
 	<div class="border-2 border-gray-900 border-dashed h-full flex flex-col p-4">
 		<div class="relative">
@@ -70,7 +214,7 @@
 			<div
 				class="absolute left-1/2 top-2 bottom-2 transform -translate-x-1/2 bg-black w-[0.125rem]"
 			/>
-			<div class="absolute top-7 right-0 bottom-8 left-0 py-1 flex flex-col justify-between">
+			<div class="absolute top-0 right-0 bottom-0 left-0 flex flex-col justify-around">
 				<div class="h-[0.175rem] w-[47.5%] bg-black" />
 				<div class="h-[0.175rem] w-[47.5%] bg-black self-end" />
 				<div class="h-[0.175rem] w-[47.5%] bg-black" />
@@ -83,7 +227,7 @@
 				<div class="h-[0.175rem] w-[47.5%] bg-black self-end" />
 			</div>
 			<div
-				class="absolute left-1/2 top-7 bottom-8 transform -translate-x-1/2 flex flex-col justify-between"
+				class="absolute left-1/2 top-0 bottom-0 transform -translate-x-1/2 flex flex-col justify-around"
 			>
 				<div class="w-3 h-3 rounded-full bg-black" />
 				<div class="w-3 h-3 rounded-full bg-black" />
@@ -97,7 +241,7 @@
 				<div class="w-3 h-3 rounded-full bg-black" />
 			</div>
 			<div
-				class="absolute top-4 right-0 bottom-4 left-0 flex flex-col justify-between font-cheap-pine-sans text-3xl tracking-widest"
+				class="absolute top-0 right-0 bottom-0 left-0 flex flex-col justify-around font-cheap-pine-sans text-3xl tracking-widest"
 			>
 				<span class="w-[47.5%] self-end">14 Uhr</span>
 				<span class="w-[47.5%] text-right">15 Uhr</span>
@@ -175,41 +319,9 @@
 		</div>
 		<hr class="border border-black my-1" />
 		<hr class="border-[3px] border-black my-1" />
-		<div class="flex items-center divide-x-2 divide-black my-3">
-			<span class="text-8xl font-cheap-pine pr-2">23</span>
-			<div class="flex flex-col text-center px-2">
-				<span
-					class="font-cheap-pine-sans text-4xl tracking-widest leading-none bg-black text-white px-1"
-					>September</span
-				>
-				<span class="font-cheap-pine-sans text-6xl tracking-wider font-bold leading-[0.85]"
-					>2 0 2 3</span
-				>
-			</div>
-			<div class="flex-grow flex flex-col text-center">
-				<span class="font-cheap-pine-sans text-4xl tracking-widest leading-none">
-					Midrène's und Josua's
-				</span>
-				<div class="flex items-center justify-between px-3">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="text-red-600 h-9 w-9"
-						viewBox="0 0 20 20"
-						fill="currentcolor"
-					>
-						<path d="M20 7h-7L10 .5 7 7H0l5.46 5.47-1.64 7 6.18-3.7 6.18 3.73-1.63-7z" />
-					</svg>
-					<span class="font-cheap-pine text-6xl tracking-widest"> Hochzeit </span>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="text-red-600 h-9 w-9"
-						viewBox="0 0 20 20"
-						fill="currentcolor"
-					>
-						<path d="M20 7h-7L10 .5 7 7H0l5.46 5.47-1.64 7 6.18-3.7 6.18 3.73-1.63-7z" />
-					</svg>
-				</div>
-			</div>
+		<div class="flex justify-between font-bold uppercase">
+			<span class="flex-1 text-left">Midrène's und Josua's Hochzeit</span>
+			<span class="flex-1 text-right">Seite 1 / 13</span>
 		</div>
 		<span class="text-center text-gray-700 tracking-widest">
 			x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;
@@ -218,3 +330,142 @@
 		</span>
 	</div>
 </div>
+
+<div class="shadow-2xl h-[56rem] w-[42rem] p-1 bg-white">
+	<div class="border-2 border-gray-900 border-dashed h-full flex flex-col p-4">
+		<div class="relative">
+			<svg
+				class="w-full"
+				version="1.1"
+				viewBox="0 0 264.58 39.688"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path fill="#000" d="m46.302 34.297-5.5562-5.5562h5.5562z" stroke-width=".39688" />
+				<path
+					transform="matrix(.28414 0 0 .28414 9.5684 -16.833)"
+					d="m24.523 149.68-6.9838 6.9838-6.9838-6.9838 6.9838-6.9838z"
+				/>
+				<g stroke-width=".39688">
+					<path d="m9.2604 17.099 8.599 8.599" fill="none" stroke="#f00" stroke-linecap="square" />
+					<path d="m9.2604 34.297 8.599-8.599" fill="none" stroke="#f00" stroke-linecap="square" />
+					<path d="m218.09 34.297 5.5562-5.5562h-5.5562z" />
+				</g>
+				<path
+					transform="matrix(-.28414 0 0 .28414 254.83 -16.833)"
+					d="m24.523 149.68-6.9838 6.9838-6.9838-6.9838 6.9838-6.9838z"
+				/>
+				<path
+					d="m255.14 17.099-8.599 8.599"
+					fill="none"
+					stroke="#f00"
+					stroke-linecap="square"
+					stroke-width=".39688"
+				/>
+				<path
+					d="m255.14 34.297-8.599-8.599"
+					fill="none"
+					stroke="#f00"
+					stroke-linecap="square"
+					stroke-width=".39688"
+				/>
+				<g stroke="#000">
+					<path
+						d="m10.583 17.099h35.719v17.198h-35.719l8.599-8.599z"
+						fill="none"
+						stroke-width=".79375"
+					/>
+					<path
+						d="m253.81 17.099h-35.719v17.198h35.719l-8.599-8.599z"
+						fill="none"
+						stroke-width=".79375"
+					/>
+					<rect
+						x="33.342"
+						y="5.461"
+						width="197.9"
+						height="23.275"
+						fill="#fff"
+						fill-rule="evenodd"
+						stroke-width=".93392"
+					/>
+				</g>
+			</svg>
+
+			<div
+				class="absolute top-[0.65rem] w-full font-cheap-pine text-6xl text-center tracking-widest"
+			>
+				Das Brautpaar
+			</div>
+		</div>
+		<hr class="border-[3px] border-black my-1" />
+		<hr class="border border-black my-1" />
+		<div class="flex-grow relative">
+			<div class="absolute right-0 left-0 top-1/2 transform -translate-y-1/2">
+				<hr class="border border-black" />
+			</div>
+			<div class="absolute right-0 left-0 top-1/2 transform -translate-y-1/2 flex justify-around">
+				{#each milestones as _}
+					<div class="w-3 h-3 rounded-full bg-black" />
+				{/each}
+			</div>
+			<div class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-around">
+				{#each milestones as _, id}
+					<div class="w-[0.175rem] h-36 bg-black {id % 2 === 0 ? 'mb-44' : 'mt-44'}" />
+				{/each}
+			</div>
+			<div
+				class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-around font-cheap-pine-sans text-3xl"
+			>
+				{#each milestones as milestone, id}
+					<div
+						class="flex flex-col text-center {id % 2 === 0
+							? 'mt-[25%]'
+							: 'mb-[25%]'} min-w-[5.5rem]"
+					>
+						<span class="text-7xl font-cheap-pine leading-[0.9]">{milestone.date.getDate()}</span>
+						<span
+							class="font-cheap-pine-sans text-xl tracking-widest leading-none bg-black text-white px-1"
+						>
+							{monthNames[milestone.date.getMonth()]}
+						</span>
+						<div class="flex justify-between font-cheap-pine-sans text-4xl font-bold">
+							{#each milestone.date.getFullYear().toString().split('') as num}
+								<span>{num}</span>
+							{/each}
+						</div>
+					</div>
+				{/each}
+			</div>
+			<div class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-around">
+				{#each milestones as milestone, id}
+					<div class="relative mb-3 mr-3 {id % 2 === 0 ? 'mb-[75%]' : 'mt-[75%]'}">
+						<div class="absolute top-2 left-2 w-full h-full bg-white border-4 border-red-600" />
+						<div
+							class="relative border-4 border-black bg-white min-w-[5.5rem] max-w-[5.5rem] min-h-[6.5rem] flex flex-col items-center ring-2 ring-white"
+						>
+							{@html milestone.image}
+							<span class="self-stretch bg-black text-white text-center">{milestone.title}</span>
+						</div>
+					</div>
+				{/each}
+			</div>
+		</div>
+		<hr class="border border-black my-1" />
+		<hr class="border-[3px] border-black my-1" />
+		<div class="flex justify-between font-bold uppercase">
+			<span class="flex-1 text-left">Midrène's und Josua's Hochzeit</span>
+			<span class="flex-1 text-right">Seite 2 / 13</span>
+		</div>
+		<span class="text-center text-gray-700 tracking-widest">
+			x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;
+			x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;
+			x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;&nbsp;&nbsp;&nbsp;x
+		</span>
+	</div>
+</div>
+
+<style lang="postcss">
+	.image {
+		background-image: url('/images/people/heads.jpg');
+	}
+</style>
