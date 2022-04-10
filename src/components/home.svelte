6910b6613b844cd0ceb0138d47d268beaeede712
Josua Frank <frank.josua@gmail.com>
2022-04-10 22:23:59 +0200

    more images compressed

--- src/components/home.svelte
+++ src/components/home.svelte
@@ -9,7 +9,7 @@
 	<div class="flex grow flex-col items-center lg:flex-row lg:justify-center lg:gap-12 xl:gap-48">
 		<div class="grid aspect-square w-[15rem] grid-cols-2 grid-rows-1 md:w-[20rem] lg:w-[40rem]">
 			<div
-				style="background-image: url('/images/people/midrene-and-josua.png')"
+				style="background-image: url('/images/people/midrene-and-josua.jpg')"
 				class="image group col-start-1 row-start-1 flex items-end justify-center overflow-hidden bg-cover bg-center bg-no-repeat transition"
 			>
 				<div
