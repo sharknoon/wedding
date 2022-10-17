191fdc9777b114f2f2364b175905ad8bac1b3348
Josua Frank <frank.josua@gmail.com>
2022-10-17 21:32:40 +0200

    finished guestbook

--- src/routes/admin/guestbook/+page.svelte
+++ src/routes/admin/guestbook/+page.svelte
@@ -2,7 +2,7 @@
 	import Site from './site.svelte';
 	import { downloadIds } from '$lib/client/stores';
 
-	$downloadIds = [1, 2, 3].map((i) => `guestbook-${i}`);
+	$downloadIds = ['guestbook-front', ...[1, 2, 3].map((i) => `guestbook-${i}`), 'guestbook-back'];
 
 	const heartBox1Texts = [
 		'Welche besonderen Erinnerungen habt ihr zusammen mit dem Brautpaar?',
@@ -32,6 +32,58 @@
 </script>
 
 <div class="flex flex-col items-center gap-16">
+	<div
+		class="h-[303mm] w-[216mm] max-w-[216mm] grid-cols-2 bg-white p-[8mm] shadow-2xl"
+		id="guestbook-front"
+	>
+		<div class="relative flex h-full flex-col border-2 border-dashed border-gray-900 p-4">
+			<h1 class="absolute top-[16%] left-1/2 mb-8 -translate-x-1/2 font-cheap-pine text-6xl">
+				Gästebuch
+			</h1>
+			<div class="flex grow flex-col items-center justify-center gap-8 px-12">
+				<div class="grid h-[17rem] w-[17rem] grid-cols-2 grid-rows-1">
+					<div
+						style="background-image: url('/images/people/midrene-and-josua-large.png')"
+						class="col-start-1 row-start-1 bg-cover bg-center bg-no-repeat"
+					/>
+					<div class="col-start-2 row-start-1 flex flex-col">
+						<div
+							class="flex flex-1 items-end justify-center text-center font-cheap-pine-sans text-4xl uppercase tracking-widest"
+						>
+							B l a c k
+						</div>
+						<div
+							class="flex-1 bg-black text-center font-cheap-pine-sans text-4xl uppercase tracking-widest text-white"
+						>
+							W h i t e
+						</div>
+					</div>
+				</div>
+				<div class="mx-12 flex items-center gap-4 self-stretch px-4">
+					<div class="grow border-t-2 border-black" />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-10 w-10"
+						viewBox="0 0 20 20"
+						fill="rgb(220,38,38)"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<div class="grow border-t-2 border-black" />
+				</div>
+				<div
+					class="pb-1 text-center font-cheap-pine-sans text-xl uppercase leading-4 tracking-wider"
+				>
+					Gesucht und Gefunden, in Liebe verbunden
+				</div>
+			</div>
+		</div>
+	</div>
+
 	{#each [1, 2, 3] as i}
 		<Site id={`guestbook-${i}`}>
 			<div slot="heart-box-1" class="flex h-full flex-col justify-between text-center">
@@ -62,6 +114,37 @@
 			</div>
 		</Site>
 	{/each}
+
+	<div
+		class="h-[303mm] w-[216mm] max-w-[216mm] grid-cols-2 bg-white p-[8mm] shadow-2xl"
+		id="guestbook-back"
+	>
+		<div class="flex h-full flex-col border-2 border-dashed border-gray-900 p-4">
+			<div class="flex grow flex-col items-center justify-center gap-8">
+				<div class="mx-12 flex items-center gap-4 self-stretch px-4">
+					<div class="grow border-t-2 border-black" />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-10 w-10"
+						viewBox="0 0 20 20"
+						fill="rgb(220,38,38)"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<div class="grow border-t-2 border-black" />
+				</div>
+				<div
+					class="pb-1 text-center font-cheap-pine-sans text-xl uppercase leading-4 tracking-wider"
+				>
+					Gesucht und Gefunden, in Liebe verbunden
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<div class="text-center">
 		216x303mm, Buch DIN A4 hoch, Umschlag: Hardcover 4/0-farbig, Inhalt: 48 farbige Innenseiten
