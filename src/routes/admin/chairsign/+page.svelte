c78dffdc965c81354c1e2735467691235b0177d4
Josua Frank <frank.josua@gmail.com>
2023-05-02 14:38:05 +0200

    added chairsign

--- src/routes/admin/chairsign/+page.svelte
+++ src/routes/admin/chairsign/+page.svelte
@@ -3,20 +3,51 @@
 
 	const downloadables: HTMLElement[] = [];
 
-	const members: string[] = ['bla', 'test'];
+	const members: string[] = [
+		'die Mutter der Braut',
+		'den Vater der Braut',
+		'Hannelore',
+		'unsere Trauzeugin',
+		'unseren Trauzeuge',
+		'die Mutter des Bräutigams',
+		'Frank',
+		'die Schwester des Bräutigams',
+		'die Schwester des Bräutigams',
+		'den Schwager des Bräutigams'
+	];
 </script>
 
 <div class="flex flex-col items-center gap-16">
 	<DownloadButton {downloadables} scale={4} />
-	{#each members as member}
-		<div
-			class="h-[216mm] w-[111mm] max-w-[111mm] bg-white p-[6mm] shadow-2xl"
-			id="tableassignments"
-			bind:this={downloadables[0]}
-		>
-			<div class="flex h-full flex-col border-4 border-dashed border-black p-6 text-center">
-				<h1 class="col-span-3 m-3 font-cheap-pine text-6xl">Reserviert</h1>
-				<div class="my-6 flex flex-col justify-between">test</div>
+	{#each members as member, i}
+		<div class="drop-shadow-2xl" id="chairsign-{i}" bind:this={downloadables[i]}>
+			<div class="clipped relative h-[216mm] w-[111mm] max-w-[111mm] bg-white p-[6mm] shadow-2xl">
+				<div
+					class="absolute right-1/2 top-[8mm] h-4 w-4 translate-x-1/2 rounded-full bg-gray-200"
+				/>
+				<svg
+					class="absolute inset-0"
+					width="111mm"
+					height="216mm"
+					version="1.1"
+					viewBox="0 0 111 216"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="m6 35 29-29h41l29 29v167l-49.5-49.5-49.5 49.5z"
+						fill="none"
+						stroke="#000"
+						stroke-dasharray="3"
+						stroke-dashoffset="1"
+					/>
+				</svg>
+
+				<div class="flex h-full flex-col justify-center p-6 pb-36 text-center">
+					<h1 class="col-span-3 m-3 font-cheap-pine-sans text-6xl">
+						Dieser Stuhl ist reserviert für
+					</h1>
+					<div class="m-3 font-cheap-pine text-7xl text-red-600">{member}</div>
+				</div>
 			</div>
 		</div>
 	{/each}
@@ -34,3 +65,19 @@
 		Datenformat: 111x216mm, Beschnitt: 3mm, Sicherheitsabstand: 3mm
 	</div>
 </div>
+
+<style>
+	.clipped {
+		--edge-length: 32mm;
+		--width: 111mm;
+		clip-path: polygon(
+			var(--edge-length) 0%,
+			calc(100% - var(--edge-length)) 0%,
+			100% var(--edge-length),
+			100% 100%,
+			50% calc(100% - calc(var(--width) / 2)),
+			0 100%,
+			0% var(--edge-length)
+		);
+	}
+</style>
