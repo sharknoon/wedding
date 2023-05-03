1b94761dace26637435d4f91f8d35e4826ed6f77
Josua Frank <frank.josua@gmail.com>
2023-05-03 13:16:57 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,8 @@ const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
-	}
+	},
+	reporter: process.env.CI ? 'github' : 'list',
 };
 
 export default config;
