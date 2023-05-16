c8ae06569531159605730c836790072aca476214
Josua Frank <frank.josua@gmail.com>
2023-05-16 16:21:59 +0200

    fix tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,4 +1,9 @@
 import { expect, test } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
+const userFirstName = faker.person.firstName();
+const userLastName = faker.person.lastName();
+const userSlug = userLastName.toLowerCase();
 
 test('index page tells the viewer to use the link of the save-the-date card', async ({ page }) => {
 	await page.goto('/');
@@ -18,32 +23,34 @@ test('index page has a input field for the invitation code which checks if the i
 test('the admin can create a invitation in the admin panel', async ({ page }) => {
 	await page.goto('/admin');
 	await page.getByText('Neue Einladung erstellen').click();
-	await page.getByPlaceholder('Liebe Erika, lieber Max').fill('Lieber Tom Tester');
-	await page.getByPlaceholder('Erika Mustermann').fill('Tom Tester');
+	await page
+		.getByPlaceholder('Liebe Erika, lieber Max')
+		.fill(`Liebe/r ${userFirstName} ${userLastName}`);
+	await page.getByPlaceholder('Erika Mustermann').fill(`${userFirstName} ${userLastName}`);
 	await page.getByText('Speichern').click();
 	await page.waitForTimeout(1000);
-	await expect(page.locator('tr:has-text("Tom Tester")')).toBeVisible();
+	await expect(page.locator(`tr:has-text("${userFirstName} ${userLastName}")`)).toBeVisible();
 });
 
 test('the new user can fill out his invitation code and see the invitation', async ({ page }) => {
 	await page.goto('/');
-	await page.fill('input', 'tester');
+	await page.fill('input', userSlug);
 	await page.keyboard.press('Enter');
-	await page.waitForURL(/.*\/tester/);
+	await page.waitForURL(`**/${userSlug}`);
 	await expect(page.getByRole('heading', { name: 'Die Hochzeit', exact: true })).toBeVisible();
 	await expect(page.getByText('Zusagen')).toBeVisible();
 	await expect(page.getByText('Absagen')).toBeVisible();
 });
 
 test('the user can decline the invite', async ({ page }) => {
-	await page.goto('/tester');
+	await page.goto(`/${userSlug}`);
 	await page.getByText('Absagen').click();
 	await page.waitForTimeout(500);
 	await expect(page.getByText('Abgesagt')).toBeVisible();
 });
 
 test('the user can still accept the invite after declining it', async ({ page }) => {
-	await page.goto('/tester');
+	await page.goto(`/${userSlug}`);
 	await page.getByRole('button', { name: 'Doch noch zusagen' }).click();
 	await page.waitForTimeout(500);
 	await expect(page.getByText('Ich esse')).toBeVisible();
@@ -65,7 +72,12 @@ test('the user can still accept the invite after declining it', async ({ page })
 test('the admin can delete a invitation in the admin panel', async ({ page }) => {
 	await page.goto('/admin');
 	page.on('dialog', (dialog) => dialog.accept());
-	await page.locator('tr:has-text("Tom Tester")').last().getByRole('button').last().click();
+	await page
+		.locator(`tr:has-text("${userFirstName} ${userLastName}")`)
+		.last()
+		.getByRole('button')
+		.last()
+		.click();
 	await page.waitForTimeout(1000);
-	await expect(page.locator('tr:has-text("Tom Tester")')).not.toBeVisible();
+	await expect(page.locator(`tr:has-text("${userFirstName} ${userLastName}")`)).not.toBeVisible();
 });
